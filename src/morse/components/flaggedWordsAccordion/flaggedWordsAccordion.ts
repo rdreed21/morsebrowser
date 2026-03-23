686571be42d4e8207d87d1df3dee7225b88fd2ee
Roger Rabbit <rdreed21@gmail.com>
2026-03-23 10:12:54 -0400

    update

--- src/morse/components/flaggedWordsAccordion/flaggedWordsAccordion.ts
+++ src/morse/components/flaggedWordsAccordion/flaggedWordsAccordion.ts
@@ -2,10 +2,14 @@ import { MorseViewModel } from '../../morse'
 import imageTemplate from './flaggedWordsAccordion.html'
 class FlaggedWordsAccordion {
   vm:MorseViewModel
+  flagImageSrc: string
+  uploadImageSrc: string
+  trashImageSrc: string
   constructor (params: any) {
     this.vm = params.root
-    // console.log(this.vm)
-    // console.log(params)
+    this.flagImageSrc = this.vm.morseLoadImages().getSrc('flagImage')
+    this.uploadImageSrc = this.vm.morseLoadImages().getSrc('uploadImage')
+    this.trashImageSrc = this.vm.morseLoadImages().getSrc('trashImage')
   }
 }
 // https://keepinguptodate.com/pages/2019/12/using-typescript-with-knockout/
